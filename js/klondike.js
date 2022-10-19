6f5b403b513c503a23b25a2f2d6316b6315a1c8f
Alexander Berson <aberson@yahoo.com>
2022-10-19 09:48:27 +0200

    place cards

--- js/klondike.js
+++ js/klondike.js
@@ -1,3 +1,112 @@
+const fieldSize = 28;
+const deckSize = 52;
+const suits = {
+    '♥': 'hearts',
+    '♦': 'diamonds',
+    '♠': 'spades',
+    '♣': 'clubs',
+} 
+
+const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]] 
+    }
+}
+
+const safari = () => {
+
+    let userAgent = window.navigator.userAgent.toLowerCase(),
+    sfri = /safari/.test(userAgent),
+    ios = /iphone|ipod|ipad/.test(userAgent);
+
+    if (sfri) return true;
+     
+    return false;
+}
+
+const fillField = (topCell, cards, offset, delay, interval, duration) => {
+
+    let opened = [0, 7, 13, 18, 22, 25, 27];
+
+    for (let i = 0; i < fieldSize; i++) {
+
+        delay += interval;
+
+        let card = cards[i];
+
+        card.style.left = topCell.offsetLeft + 'px';
+        card.style.top = topCell.offsetTop +  offset + 'px';
+
+        card.style.display = 'block';
+
+        let cell = document.querySelectorAll('.cell')[i + 7];
+        let offsetLeft = cell.offsetLeft - card.offsetLeft;
+        let offsetTop = cell.offsetTop - card.offsetTop;
+
+        card.querySelectorAll('.front, .back').forEach(card => {
+            card.style.transition = `all ${duration - 0.1}s ${delay + 0.1}s linear`;
+        });
+
+        card.style.transition = `all ${duration}s ${delay}s linear, opacity 0s linear`;
+        card.style.opacity = 1;
+        if (opened.includes(i)) card.classList.toggle('flip');
+        card.style.transform = `translate(${offsetLeft - 2}px, ${offsetTop}px)`;
+    }
+
+    return delay;
+}
+
+const fillStock = (topCell, cards, offset, delay, interval, duration) => {
+
+    let stockCell = document.querySelector('.stock');
+
+    for (let i = fieldSize; i < deckSize; i++) {
+
+        delay += interval;
+
+        let card = cards[i];
+
+        card.style.left = topCell.offsetLeft + 'px';
+        card.style.top = topCell.offsetTop +  offset + 'px';
+
+        card.style.display = 'block';
+
+        let offsetLeft = stockCell.offsetLeft - card.offsetLeft;
+        let offsetTop = stockCell.offsetTop - card.offsetTop;
+
+        card.style.transition = `all ${duration}s ${delay}s linear, opacity 0s linear`;
+        card.style.opacity = 1;
+        card.style.transform = `translate(${offsetLeft - 2}px, ${offsetTop}px)`;
+    }
+
+    return delay;
+}
+
+// const fillStock = (topCell, cards, offset, delay, interval, duration) => {
+
+//     let stockCell = document.querySelector(".stock");
+
+//     for (let i = pyramidSize; i < deckSize - 1; i++) {
+
+//         delay += interval;
+
+//         let card = cards[i];
+
+//         card.style.left = topCell.offsetLeft + "px";
+//         card.style.top = topCell.offsetTop +  offset + "px";
+
+//         let offsetLeft = stockCell.offsetLeft - card.offsetLeft;
+//         let offsetTop = stockCell.offsetTop - card.offsetTop;
+
+//         card.style.transition = `all ${duration}s ${delay}s linear, opacity 0s linear`;
+//         card.style.opacity = 1;
+//         card.style.transform = `translate(${offsetLeft - 2}px, ${offsetTop}px)`;
+//     }
+
+//     return delay;
+// }
+
 const setCardsSize = () => {
 
     if (window.innerHeight > window.innerWidth) {
@@ -13,8 +122,150 @@ const setCardsSize = () => {
     }
 }
 
+const getDeck = () => {
+
+    let deck = [];
+    let ranks = decks[Math.floor(Math.random() * decks.length)];
+    let suits = ['♥','♠','♦','♣'];
+
+    ranks.forEach(rank => {
+
+        switch(rank) {
+            case 11:
+                rank = 'J';
+                break;
+            case 12:
+                rank = 'Q';
+                break;
+            case 13:
+                rank = 'K';
+                break;
+            case 1:
+                rank = 'A';
+                break;
+            default:
+                rank = String(rank);
+        }
+
+        shuffle(suits);
+
+        for (suit of suits) {
+
+            let card = rank + suit;
+
+            if (!deck.includes(card)) {
+                deck.push(card);
+                break;
+            }
+        }
+    });
+
+    return deck;
+}
+
+const setCards = () => {
+
+    let cards = document.querySelectorAll('.front');
+
+    deck = getDeck();
+
+    for (let i = 0; i < 52; i++){
+
+        let card = cards[i];
+        let rank = deck[i].length == 2 ? deck[i][0] : deck[i][0] + deck[i][1];
+        let suit = deck[i].length == 2 ? deck[i][1] : deck[i][2];
+
+        if (suit == '♥' || suit == '♦') { 
+            card.classList.add('red');
+        }
+
+        card.querySelector('.rank').innerText = rank;
+        // card.style.zIndex = -1;
+
+        switch(suit){
+            case '♥':
+                card.querySelector('.suit').firstElementChild.src = 'images/suits/hearts.png';
+                card.querySelector('.main').firstElementChild.src = 'images/suits/hearts.png';
+                break;
+            case '♦':
+                card.querySelector('.suit').firstElementChild.src = 'images/suits/diamonds.png';
+                card.querySelector('.main').firstElementChild.src = 'images/suits/diamonds.png';
+                break;
+            case '♠':
+                card.querySelector('.suit').firstElementChild.src = 'images/suits/spades.png';
+                card.querySelector('.main').firstElementChild.src = 'images/suits/spades.png';
+                break;
+            case '♣':
+                card.querySelector('.suit').firstElementChild.src = 'images/suits/clubs.png';
+                card.querySelector('.main').firstElementChild.src = 'images/suits/clubs.png';
+                break;
+        }
+    } 
+}
+
 const setTable = () => {
+
     setCardsSize();
+    setCards();
+
+    let topCell = document.querySelectorAll('.cell')[3];
+    let cards =  document.querySelectorAll('.card-wrap');
+
+    setTimeout(() => {
+
+        let offsetPlus = safari() ? 10 : 50;
+        let offset = window.innerHeight - topCell.parentNode.parentNode.offsetTop + offsetPlus;
+
+        let delay = 0;
+        let interval = 0.05;
+        let duration = 0.5;
+
+        delay = fillStock(topCell, cards, offset, delay, interval, duration);
+        delay = fillField(topCell, cards, offset, delay, interval, duration);
+
+    }, 500);
+}
+
+const enableCard = (card) => {
+
+    card = card.currentTarget ? card.currentTarget : card;
+
+    card.removeEventListener('transitionend', enableCard); 
+    card.firstElementChild.classList.remove("zoom");
+    
+    if (touchScreen()){
+        card.addEventListener("touchstart", move);
+        card.addEventListener("touchend", removeZoom);
+        card.addEventListener("touchcancel", removeZoom);
+    } else {
+        card.addEventListener("mousedown", move);
+        card.addEventListener("mouseup", removeZoom);
+        card.addEventListener("mouseleave", removeZoom);
+    }
+}
+
+const enableCards = () => {
+    for (let card of document.querySelectorAll('.card-wrap')){
+        enableCard(card);
+    }
+}
+
+const disableCard = (card) => {
+    if (touchScreen()){
+        card.removeEventListener("touchstart", move);
+        card.removeEventListener("touchend", removeZoom);
+        card.removeEventListener("touchcancel", removeZoom);
+    } else {
+        card.removeEventListener("mousedown", move);
+        card.removeEventListener("mouseup", removeZoom);
+        card.removeEventListener("mouseleave", removeZoom);
+    }
+} 
+
+const disableCards = () => {
+    for (let card of document.querySelectorAll('.card-wrap')){
+        disableCard(card);
+    }
 }
 
 const disableTapZoom = () => {
@@ -27,7 +278,7 @@ const disableTapZoom = () => {
 
 const init = () => {
 
-    // disableTapZoom();
+    disableTapZoom();
 
     // disableCards();
 
@@ -36,8 +287,4 @@ const init = () => {
     // setTimeout(enableCards, 3700);    
 }
 
-window.onload = () => {
-    document.fonts.ready.then(() => {
-        setTimeout(init, 0); 
-    });
-}
+window.onload = () => document.fonts.ready.then(() => init());
